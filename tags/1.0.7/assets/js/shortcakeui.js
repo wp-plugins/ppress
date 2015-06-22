9d508f28a8c062d9ac916636b2b57eb0f2e0b7d0
Collizo4sky <Collizo4sky@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-22 15:25:14 +0000

    bumped to version 1.0.7
    git-svn-id: https://plugins.svn.wordpress.org/ppress@1185601 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/1.0.7/assets/js/shortcakeui.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function($) {
+    $('#pp-insert-shortcake').click(function(){
+        var wp_media_frame = wp.media.frames.wp_media_frame = wp.media({
+            frame : "post",
+            state : 'shortcode-ui'
+        });
+
+        wp_media_frame.open();
+    });
+});
